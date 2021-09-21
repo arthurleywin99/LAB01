bf7e01823ee5566edacc7ebd24396211a3225166
Bùi Giang <truonggiang.1112@icloud.com>
2021-09-21 21:20:18 +0700

    Thêm Nhà phố và Chung cư

---
+++ LAB01_04/Apartment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB01_04
+{
+    internal class Apartment
+    {
+
+    }
+}
